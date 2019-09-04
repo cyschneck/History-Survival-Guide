7e2163a8c675a7862b97d20c82f42fa3ddb3d227
cyschneck <cora.schneck@gmail.com>
2019-09-03 18:09:56 -0600

    page_1 proper motion with angular distance and README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# History-Survival-Guide
+# A Time Traveler’s Guide to Surviving History
+## A History Survival Guide
+![Banner_Light](https://user-images.githubusercontent.com/22159116/64215868-b84b1500-ce73-11e9-98fc-4cac0c190fc4.jpg)
+
+Relevant code behind a given guidebook page
+
+[historysurvivalguide.com](http://historysurvivalguide.com/)
+
+### [Page 1: Proper Motion](https://github.com/cyschneck/History-Survival-Guide/page_1_proper_motion/)
+Determine Angular Motion Between Two Given Stars
---
+++ page_1_proper_motion/angular_distance.py
@@ -0,0 +1,82 @@
+# Calculate the angular distance between two stars (S1, S2)
+import math # math.cos
+
+def declinationinDecimalDegrees(star_declination):
+	# convert declination from hours to degrees
+	star_declination_split = star_declination.split('.')
+	degrees = star_declination_split[0]
+	is_negative_degree = False
+	if '-' in degrees:
+		is_negative_degree = True
+		degrees = degrees.strip('-')
+	arcminutes = star_declination_split[1][:2]
+	arcseconds = star_declination_split[1][2:]
+
+	converted_declination = float(degrees) + float(float(arcminutes)/60) + float(float(arcseconds)/3600)
+	if is_negative_degree:
+		converted_declination *= -1
+	return converted_declination
+
+def rightAscensioninDegrees(star_ra):
+	# convert right ascension from hours to degrees
+	star_ra_split = star_ra.split('.')
+	hours = star_ra_split[0]
+	minutes = star_ra_split[1][:2]
+	seconds = star_ra_split[1][2:]
+
+	converted_ra = float(hours) + float(float(minutes)/60) + float(float(seconds)/3600)
+	return converted_ra
+
+def calculateAngularDistance(s1_ra, s1_declination, s2_ra, s2_declination):
+	# calculate angular distance between two stars
+	cos_a =  math.sin(math.radians(s1_declination))*math.sin(math.radians(s2_declination))
+	cos_a += math.cos(math.radians(s1_declination))*math.cos(math.radians(s2_declination))*math.cos(math.radians(15*(s1_ra - s2_ra)))
+	print("cos(A) = {0}".format(cos_a))
+	angular_distance = math.acos(cos_a)
+
+	print("Angular Distance = {0} in radians".format(angular_distance))
+	print("Angular Distance = {0} in degrees".format(math.degrees(angular_distance)))
+
+if __name__ == '__main__':
+	# Replace star with name and right ascension/declination
+	# Properties of stars can be found: https://in-the-sky.org/
+	s1 = "Megrez"
+	s1_ra = '12.1525' # hours.minutesecond (HH.MMSS)
+	s1_ra = rightAscensioninDegrees(s1_ra)
+	s1_declination =  '57.0157' # degree.arcminutearcsecond (DD.MMSS)
+	s1_declination = declinationinDecimalDegrees(s1_declination)
+	print("{0}'s right ascension in decimal hours = {1}".format(s1, s1_ra))
+	print("{0}'s declination in decimal degrees = {1}".format(s1, s1_declination))
+
+	s2 = "Dubhe"
+	s2_ra = '11.0343' # hours.minutesecond (HH.MMSS)
+	s2_ra = rightAscensioninDegrees(s2_ra)
+	s2_declination = '61.4504' # degree.arcminutearcsecond (DD.MMSS)
+	s2_declination = declinationinDecimalDegrees(s2_declination)
+	print("{0}'s right ascension in decimal hours = {1}".format(s2, s2_ra))
+	print("{0}'s declination in decimal degrees = {1}".format(s2, s2_declination))
+
+	print("\n")
+	calculateAngularDistance(s1_ra, s1_declination, s2_ra, s2_declination)
+
+	'''
+	# ADDITIONAL EXAMPLE:
+	s1 = "Mimosa"
+	s1_ra = '12.4743' # hours.minutesecond (HH.MMSS)
+	s1_ra = rightAscensioninDegrees(s1_ra)
+	s1_declination =  '-59.4119' # degree.arcminutearcsecond (DD.MMSS)
+	s1_declination = declinationinDecimalDegrees(s1_declination)
+	print("{0}'s right ascension in decimal hours = {1}".format(s1, s1_ra))
+	print("{0}'s declination in decimal degrees = {1}".format(s1, s1_declination))
+
+	s2 = "Delta-Crucis"
+	s2_ra = '12.1508' # hours.minutesecond (HH.MMSS)
+	s2_ra = rightAscensioninDegrees(s2_ra)
+	s2_declination = '-58.4456' # degree.arcminutearcsecond (DD.MMSS)
+	s2_declination = declinationinDecimalDegrees(s2_declination)
+	print("{0}'s right ascension in decimal hours = {1}".format(s2, s2_ra))
+	print("{0}'s declination in decimal degrees = {1}".format(s2, s2_declination))
+
+	print("\n")
+	calculateAngularDistance(s1_ra, s1_declination, s2_ra, s2_declination)
+	'''
