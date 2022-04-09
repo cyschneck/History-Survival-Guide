443d9dbd4f3eaad40933bd6029ecbd605017235e
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-04-09 16:46:13 -0600

    plot circular chart 0-23 ra, -90-90 declination

--- page_x_astrolabe/generate_star_chart.py
+++ page_x_astrolabe/generate_star_chart.py
@@ -3,17 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-if __name__ == '__main__':
-	fig = plt.figure(figsize=(5, 5), dpi=120)
-	gs = gridspec.GridSpec(1, 1)
-	ax = plt.subplot(gs[0], polar=True)
-	ax.set_ylim(-90, -55)
+def convertRAtoRadians(ra_in_degrees):
+	# convert RA from degrees to radians
+	ra_in_radains = ra_in_degrees / 180.0 * np.pi
+	return ra_in_radains
+
+def plotCircluar():
+	# plot star chart as a circular graph
+	fig = plt.figure(figsize=(8,8), dpi=120)
+	ax = fig.subplots(subplot_kw={'projection': 'polar'})
 
 	# Set Right Ascension (astronomical 'longitude') as X
-	angs = np.array([330, 345, 0, 15, 30, 45, 60, 75, 90, 105, 120,
-					135, 150,  165, 180, 195, 210, 225, 240, 255,
-					270, 285, 300, 315])
-	plt.xticks(angs * np.pi / 180., fontsize=10)
+	angles_ra = np.array([330, 345, 0, 15, 30, 45, 60, 75, 90, 105, 120,
+						135, 150,  165, 180, 195, 210, 225, 240, 255,
+						270, 285, 300, 315])
+	plt.xticks(angles_ra * np.pi / 180, fontsize=8)
 	ax.set_xticklabels(['$22^h$', '$23^h$', '$0^h$', '$1^h$', '$2^h$',
 						'$3^h$', '$4^h$', '$5^h$','$6^h$',
 						'$7^h$',
@@ -33,9 +37,13 @@ if __name__ == '__main__':
 						'$21^h$'], fontsize=10)
 	
 	# Set Declination (astronomical 'latitude') as Y
-	plt.yticks(np.arange(-80, -59, 10), fontsize=10)
-	ax.set_rlabel_position(120)
-	ax.set_yticklabels(['$-80^{\circ}$', '$-70^{\circ}$', '$-60^{\circ}$'],
-		fontsize=10)
-		
+	ax.set_ylim(-90, 90)
+	declination_values = np.arange(-90, 91, 10)
+	plt.yticks(declination_values, fontsize=5)
+	#ax.set_rlabel_position(120)
+	#ax.set_yticklabels(['$-80^{\circ}$', '$-70^{\circ}$', '$-60^{\circ}$'],
+	#	fontsize=10)
 	plt.show()
+
+if __name__ == '__main__':
+	plotCircluar()
