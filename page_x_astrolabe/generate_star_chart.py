04fa39b8c5aca624889da532f7c846aa7cb5119b
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-04-10 15:05:10 -0600

    display declination optionally, add southern stars

--- page_x_astrolabe/generate_star_chart.py
+++ page_x_astrolabe/generate_star_chart.py
@@ -39,10 +39,12 @@ def plotCircluar(star_list, displayStarNamesLabels, displayDeclinationMarks):
 	# Set Declination (astronomical 'latitude') as Y
 	ax.set_ylim(-90, 90)
 	declination_values = np.arange(-90, 91, 10)
-	#if displayDeclinationMarks:
-	plt.yticks(declination_values, fontsize=5)
-	ax.set_rlabel_position(120)
-	#ax.set_yticklabels(['$-80^{\circ}$', '$-70^{\circ}$', '$-60^{\circ}$'], fontsize=10)
+	if displayDeclinationMarks:
+		plt.yticks(declination_values, fontsize=5)
+		#ax.set_yticklabels(['$-80^{\circ}$', '$-70^{\circ}$', '$-60^{\circ}$'], fontsize=10)
+		ax.set_rlabel_position(120)
+	else:
+		plt.yticks(declination_values, fontsize=0)
 	
 	# convert to x and y values for stars
 	x_star_labels = []
@@ -54,32 +56,65 @@ def plotCircluar(star_list, displayStarNamesLabels, displayDeclinationMarks):
 		y_dec_values.append(star[2])
 	ax.scatter(x_ra_values, y_dec_values)
 
-	# label stars
+	# label stars (optional)
 	if displayStarNamesLabels:
 		for i, txt in enumerate(x_star_labels):
 			ax.annotate(txt, (x_ra_values[i], y_dec_values[i]))
+
 	plt.show()
 	fig.savefig('star_chart.png', dpi=fig.dpi)
 
 if __name__ == '__main__':
 	# stars to be included: 'name', ra HH.MM.SS, declination DD.SS
+	# Northern stars
 	sirus_star = ["Sirus", "06.45.08", -16.42]
 	dubhe_star = ["Dubhe", "11.03.43", 61.45]
 	megrez_star = ["Megrez", "12.15.25", 57.01]
 	polaris_star = ["Polaris", "02.31.49", 89.15]
+	betelgeuse_star = ["Betelgeuse", "05.55.10", 7.24]
+	rigel_star = ["Rigel", "05.14.32", -8.12]
+	spica_star = ["Spica", "13.25.11", -11.09]
+	vega_star = ["Vega", "18.36.56", 38.47]
+	altair_star = ["Altair", "19.50.46", 8.52]
+	procyon_star = ["Procyon", "07.39.18", 5.13]
+	pollux_star = ["Pollux", "07.45.18", 28.01]
+	castor_star = ["Castor", "07.34.35", 31.53]
+	arcturus_star = ["Arcturus", "14.15.39", 19.10]
+	#Southern stars
+	mimosa_star = ["Mimosa", "12.47.43", -59.41]
+	gamma_phoenics_star = ["Gamma Phoenics", "01.28.21", -43.19]
+	beta_hydri_star = ["Beta Hydri", "00.25.45", -77.15]
+	
+	
 
 	# add stars to total star list
-	star_chart_list = []
-	star_chart_list.append(sirus_star)
-	star_chart_list.append(dubhe_star)
-	star_chart_list.append(megrez_star)
-	star_chart_list.append(polaris_star)
-	
+	northern_star_chart_list = [sirus_star,
+								dubhe_star,
+								megrez_star,
+								polaris_star,
+								betelgeuse_star,
+								rigel_star,
+								spica_star,
+								vega_star,
+								altair_star,
+								procyon_star,
+								pollux_star,
+								castor_star,
+								arcturus_star]
+
+	southern_star_chart_list = [mimosa_star,
+								gamma_phoenics_star,
+								beta_hydri_star]
+
+	star_chart_list = northern_star_chart_list + southern_star_chart_list
+
 	# Convert Star chart from RA hours to Radians to chart
-	print(star_chart_list)
+	#print(star_chart_list)
 	star_chart_list = convertRAhrtoRadians(star_chart_list)
-	print(star_chart_list)
+	#print(star_chart_list)
 
 	displayStarNames = True # display chart with star names
-	displayDeclinationMarks = False # display declination marks
+	displayDeclinationMarks = True # display declination marks
+	northOrSouth = "Both" # options: "North", "South", "Both" (changes the declination range)
+
 	plotCircluar(star_chart_list, displayStarNames, displayDeclinationMarks)
