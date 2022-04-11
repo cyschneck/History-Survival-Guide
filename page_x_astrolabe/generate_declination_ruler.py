bc3a948f16ab1b1df94eaf616c47e6f88bc9fa17
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-04-11 14:10:33 -0600

    convert segements into length segements

--- page_x_astrolabe/generate_declination_ruler.py
+++ page_x_astrolabe/generate_declination_ruler.py
@@ -2,27 +2,40 @@
 # python3 generate_declination_ruler.py
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-def rulerLengthSegements():
-	# define the length of each segement in ruler when r = 1
-	import math
+def rulerLengthSegements(graphPlotSegements):
+	# define the length of each segement in ruler when radius = 1
 	x_degreeSegements = np.arange(1,18,1)
 	y_lengthSegements = []
-	
-	for segement in x_degreeSegements:
-		segement_in_radains = (segement * np.pi / 180)
-		print(math.tan(5*segement_in_radains))
-		y_lengthSegements.append(math.tan(5*segement_in_radains))
 
+	# convert segements into length segements
+	for segement_in_circle in x_degreeSegements:
+		angle_of_inclination = 90 - (segement_in_circle*10)
+		segement_in_radians = (segement_in_circle * np.pi / 180)
+		equation_of_length = math.tan(5*segement_in_radians)
+		#equation_of_length = math.tan((90-angle_of_inclination))/2
+		print("Segement: {0} = {1} = {2}".format(segement_in_circle, angle_of_inclination, equation_of_length))
+		#y_lengthSegements.append(equation_of_length)
+
+	# optional graph of segements
+	if graphPlotSegements:
+		plotLengthSegements(x_degreeSegements, y_lengthSegements)
+
+	# calculate the ruler
+	#calculateRuler(y_lengthSegements)
+
+def plotLengthSegements(x_degreeSegements, y_lengthSegements):
+	# plot Segements (n) vs. Length of Segements
 	plt.xticks(x_degreeSegements)
 	plt.title("Length of Declination Ruler Segements: tan(5n)")
 	plt.xlabel("Degree Segements")
 	plt.ylabel("Length of Segement when r = 1")
 	plt.scatter(x_degreeSegements, y_lengthSegements)
-	#plt.show()
-	calculateRuler(y_lengthSegements)
+	plt.show()
 
 def calculateRuler(rulerSegements):
+	# Calculate the ruler based on a ruler length and the range of the declinations
 	ruler_length = 15
 	declination_range_min = -30
 	declination_range_max = 70
@@ -44,4 +57,6 @@ def calculateRuler(rulerSegements):
 	print(ratio_x)
 
 if __name__ == '__main__':
-	rulerLengthSegements()
+	# Generate and plot
+	graphPlotSegements = False # should plot the segements on a graph
+	rulerLengthSegements(graphPlotSegements)
