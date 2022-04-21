7a00ebd31967d34e9b26fc488b9c348698bf830e
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-04-20 18:15:18 -0600

    split calculations based on north/south hemisphere

--- page_x_astrolabe/generate_latitude_plate.py
+++ page_x_astrolabe/generate_latitude_plate.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import math
 
-def plotLatitudePlate(ruler_length, displayDeclination):
+def plotLatitudePlate(ruler_length, northOrSouth, displayDeclination):
 	# Plot latitude plate
 	fig = plt.figure(figsize=(12,12), dpi=100)
 	ax = fig.subplots(subplot_kw={'projection': 'polar'})
@@ -23,17 +23,24 @@ def plotLatitudePlate(ruler_length, displayDeclination):
 	equator = 0.0 # degrees
 	
 	# Calculate radius for tropics and equator
-	capricorn_radius = radius_of_plate
-	cancer_radius = radius_of_plate / math.tan(np.deg2rad(45-(tropic_of_capricorn)/2))
-	equator_radius = cancer_radius * math.tan(np.deg2rad(45-tropic_of_cancer/2))
+	if northOrSouth == "North":
+		capricorn_radius = radius_of_plate
+		equator_radius = radius_of_plate / math.tan(np.deg2rad(45-(tropic_of_capricorn)/2))
+		cancer_radius = equator_radius * math.tan(np.deg2rad(45-tropic_of_cancer/2))
+	if northOrSouth == "South":
+		cancer_radius = radius_of_plate
+		equator_radius = radius_of_plate / math.tan(np.deg2rad(45-(tropic_of_capricorn)/2))
+		capricorn_radius = equator_radius * math.tan(np.deg2rad(45-tropic_of_cancer/2))
 
 	# Display Declination lines
 	print("Tropic of Capricorn radius ({0}) = {1}".format(45-(tropic_of_capricorn)/2, capricorn_radius))
-	print("Tropic of Cancer radius ({0}) = {1:.4f}".format(45-tropic_of_cancer/2, cancer_radius))
 	print("Equator radius = {0:.4f}".format(equator_radius))
-	declination_position = [capricorn_radius, cancer_radius, equator_radius]
+	print("Tropic of Cancer radius ({0}) = {1:.4f}".format(45-tropic_of_cancer/2, cancer_radius))
+	declination_labels = ["Tropic of Capricorn", "Equator", "Tropic of Cancer"]
+	declination_position = [capricorn_radius, equator_radius, cancer_radius]
 	if displayDeclination:
 		plt.yticks(declination_position, fontsize=7)
+		ax.set_yticklabels(declination_labels, horizontalalignment="left", verticalalignment="bottom")
 	else:
 		plt.yticks(declination_position, fontsize=0) # do not display
 	#ax.set_yticklabels(declination_position)
@@ -43,7 +50,7 @@ def plotLatitudePlate(ruler_length, displayDeclination):
 	for dec in declination_position:
 		for curve in [[[0, 360], [dec, dec]]]: # draw a circle in 360 degrees
 			curve[0] = np.deg2rad(curve[0])
-			x = np.linspace(curve[0][0], curve[0][1], num=10)
+			x = np.linspace(curve[0][0], curve[0][1], num=360)
 			y = interp1d(curve[0], curve[1])(x)
 			ax.plot(x, y, color="black")
 
@@ -52,8 +59,8 @@ def plotLatitudePlate(ruler_length, displayDeclination):
 
 if __name__ == '__main__':
 	# Chart Options
-	#northOrSouth = "North" # options: "North", "South", "Full" (changes the declination range)
+	northOrSouth = "North" # options: "North", "South"
 	displayDeclination = True # display chart with RA (False/True)
 	total_ruler_length = 30 # units (cut in half for each side of the ruler) (currently has to be even)
 
-	plotLatitudePlate(total_ruler_length, displayDeclination)
+	plotLatitudePlate(total_ruler_length, northOrSouth, displayDeclination)
