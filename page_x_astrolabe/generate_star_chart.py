7b576c94ca882792519c476de79e0e34e620f65f
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-07-03 17:03:00 -0600

    flip over RA when cross 90 or -90 for proper motion

--- page_x_astrolabe/generate_star_chart.py
+++ page_x_astrolabe/generate_star_chart.py
@@ -95,7 +95,7 @@ def plotCircluar(star_list, northOrSouth, year_date_YYYY, displayStarNamesLabels
 		# returns calculated RA and Declination
 		current_year = 2022
 		time_since_current_year = year_date_YYYY - current_year # postive = future, negative = past
-		print("{0} Years".format(time_since_current_year))
+		print("Proper Motion for {0} Years".format(time_since_current_year))
 		#print("Date {0}, RA = {1}, Dec = {2}, PM Speed = {3}, PM Angle = {4}".format(year_date_YYYY, star_ra, star_dec, star_pm_speed, star_pm_angle))
 
 		star_pm_speed_degrees = 0.00000027777776630942 * star_pm_speed # convert mas/yr to degrees/yr
@@ -109,62 +109,59 @@ def plotCircluar(star_list, northOrSouth, year_date_YYYY, displayStarNamesLabels
 		#print("(RA) x Difference = {0} (rad/yr)".format(ra_x_difference_component))
 		#print("(DEC) y Difference = {0} rad/yr ({1} degrees/yr)".format(dec_y_difference_component, np.rad2deg(dec_y_difference_component)))
 
-		star_adjusted_ra = star_ra + ra_x_difference_component # in radians
-		#star_ra = np.deg2rad(15)
-		#star_adjusted_ra = np.deg2rad(95)
-		star_adjusted_declination = star_dec + np.rad2deg(dec_y_difference_component) # in degrees
-		#star_dec = 55
-		#star_adjusted_declination = 155
+		star_adjusted_ra = star_ra + ra_x_difference_component # in radians with proper motion (potentionally will be flipped 180 based on new declination)
+		#star_ra = np.deg2rad(15) # original (TEST)
+		#star_adjusted_ra = np.deg2rad(90) # new with proper motions (TEST)
+		star_adjusted_declination = star_dec + np.rad2deg(dec_y_difference_component) # in degrees new with proper motion
+		#star_dec = 30 # orignal (TEST)
+		#star_adjusted_declination = -360 # new with proper motion (TEST)
 
+		dec_x = star_adjusted_declination
 		# remap within -90 and 90 for postive declinations
-		# TODO: negative declinations
-		'''dec_x = star_adjusted_declination % 360
-		print(dec_x)
-		if dec_x > 90: # 
+		if star_adjusted_declination > 0: # Postive declinations
+			dec_x = star_adjusted_declination % 360
 			# map from 0 to 90 (postive declinations)
-			if dec_x < 180: 
+			if dec_x > 90 and dec_x <= 180: 
 				dec_x = 90 + (90 - dec_x)
 			# map from 0 to -90 (negative declinations)
-			if dec_x < 270 and dec_x > 180:
+			if dec_x <= 270 and dec_x > 180:
 				dec_x = 180 - dec_x
 			if dec_x < 360 and dec_x > 270:
 				dec_x = -90 + (dec_x - 270)
-			print(dec_x)
+		if star_adjusted_declination < -0: # Negative declinations
+			dec_x = star_adjusted_declination % -360
+			# map from 0 to -90 (negative declinations)
+			if dec_x < -90 and dec_x >= -180: 
+				dec_x = -90 - (90 + dec_x)
+			# map from 0 to 90 (postive declinations)
+			if dec_x >= -270 and dec_x <= -180:
+				dec_x = 180 + dec_x
+				dec_x = abs(dec_x)
+			if dec_x > -360 and dec_x < -270:
+				dec_x = 90 + (dec_x + 270)
+		#print("New mapped dec = {0}".format(dec_x))
+		#print("Original Dec = {0}, New Dec = {1}".format(star_dec, star_adjusted_declination))
 
 		# flip over RA by rotating 180
+		is_flipped = False
+		star_over_ninety_ra = star_adjusted_declination
+		if star_over_ninety_ra >= 0: # postive declinations
+			while (star_over_ninety_ra > 90):
+				star_over_ninety_ra -= 90
+				is_flipped = not is_flipped # flip across the center by 180
+		if star_over_ninety_ra < 0: # negative declinations
+			while (star_over_ninety_ra < -90):
+				star_over_ninety_ra += 90
+				is_flipped = not is_flipped # flip across the center by 180
+		#print("Original RA = {0}, New RA = {1}, Flipped = {2} (if true +180?)".format(np.rad2deg(star_ra), np.rad2deg(star_adjusted_ra), is_flipped))
 
-		print("Original = {0}, New = {1}".format(star_dec, star_adjusted_declination))
-		#print("star RA = {0}".format(np.rad2deg(star_adjusted_ra)))
-		
-		def quadrant(star_ra):
-			print("RA = {0}".format(star_ra))
-			star_ra = star_ra%360
-			#print(star_ra%90)
-			#print(star_ra%180)
-			#print(star_ra%270)
-			#print(star_ra%360)
-			if star_ra%90 == star_ra:
-				return 1
-			if star_ra%180 == star_ra:
-				return 2
-			if star_ra%270 == star_ra:
-				return 3
-			if star_ra%360 == star_ra:
-				return 4
-			else:
-				return 1
-		
-		# compare original quandrant of RA with the new declination's quadrant
-		print("Original Quad = {0}".format(quadrant(np.rad2deg(star_ra))))
-		print("Ajdusted Dec Quad = {0}".format(quadrant(star_adjusted_declination)))
-				
-		#star_adjusted_ra = star_adjusted_ra + np.deg2rad(180)
-		#star_adjusted_ra_flip = star_adjusted_ra + (flip_amount * np.deg2rad(180)) 
-		#print("star RA Flipped ({0} times)= {1}".format(flip_amount, np.rad2deg(star_adjusted_ra_flip)))
+		# If declination goes over ninety, flip over by 180
+		if is_flipped:
+			star_adjusted_ra = star_adjusted_ra + np.deg2rad(180)
 
-		star_adjusted_declination = dec_x'''
-		#print("Final RA: {0}".format(star_adjusted_ra))
-		#print("Final Dec: {0}".format(star_adjusted_declination))
+		star_adjusted_declination = dec_x
+		#print("Final RA: {0} degrees".format(np.rad2deg(star_adjusted_ra)))
+		#print("Final Dec: {0} degrees ".format(star_adjusted_declination))
 		return star_adjusted_ra, star_adjusted_declination
 
 	print("\nRange of Declination: {0} to {1}".format(min_dec_value, max_dec_value))
@@ -225,12 +222,12 @@ if __name__ == '__main__':
 	deneb_star = ["Deneb", "20.41.25", 45.16, 2.7, 47.4]
 	denebola_star = ["Denebola", "11.49.03", 14.34, 510.7, 257.0]
 	dubhe_star = ["Dubhe", "11.03.43", 61.45, 138.5, 255.5] # Big Dipper
-	gamma_cassiopeiae_star = ["Gamma Cassiopeiae", "00.56.42", 60.43, 25.5, 98.9] # Gamma Cassiopeiae
 	hamal_star = ["Hamal", "02.07.10", 23.27, 239.7, 128.1]
 	megrez_star = ["Megrez", "12.15.25", 57.01, 104.3, 85.5] # Big Dipper
 	merak_star = ["Merak", "11.01.50", 56.22, 88.0, 67.6] # Big Dipper
 	muphrid_star = ["Muphrid", "13.54.41", 18.23, 361.5, 189.7]
 	mizar_star = ["Mizar", "13.23.55", 54.55, 124.6, 100.5] # Big Dipper
+	navi_star = ["Navi", "00.56.42", 60.43, 25.5, 98.9] # Gamma Cassiopeiae
 	pleiades_celaeno_star = ["Celaeno", "03.44.48", 24.17, 49.2, 156.2]
 	polaris_star = ["Polaris", "02.31.49", 89.15, 46.0, 104.9]
 	pollux_star = ["Pollux", "07.45.18", 28.01, 628.2, 265.8]
@@ -239,7 +236,7 @@ if __name__ == '__main__':
 	rasalhague_star = ["Rasalhague", "17.34.56", 12.33, 246.5, 154.0]
 	regulus_star = ["Regulus", "10.08.22", 11.58, 248.8, 271.3]
 	ruchbah_star = ["Ruchbah", "01.25.49", 60.14, 300.5, 99.4] # Delta Cassiopeiae
-	schedar_star = ["Schedar", "00.04.30", 56.32, 58.4, 122.7] # Alpha Cassiopeiae
+	schedar_star = ["Schedar", "00.40.30", 56.32, 58.4, 122.7] # Alpha Cassiopeiae
 	segin_star = ["Segin", "01.54.23", 63.4, 37.3, 120.5] # Epsilon Cassiopeiae
 	seginus_star = ["Seginus", "14.32.04", 38.18, 190.4, 322.6]
 	spica_star = ["Spica", "13.25.11", -11.09, 52.3, 234.1]
@@ -285,7 +282,7 @@ if __name__ == '__main__':
 								cor_caroli_star,
 								denebola_star,
 								dubhe_star,
-								gamma_cassiopeiae_star,
+								navi_star,
 								hamal_star,
 								megrez_star,
 								merak_star,
@@ -336,7 +333,7 @@ if __name__ == '__main__':
 	northOrSouth = "North" # options: "North", "South", "Full" (changes the declination range)
 	total_ruler_length = 30 # units (cut in half for each side of the ruler) (currently has to be even)
 	increment_by = 5 # increment degrees by 1, 5, 10)
-	year_of_plate_YYYY = 1969 #2022 - 1600000 #1969 # B.C.E or written as: 2022 - 150 # years
+	year_of_plate_YYYY = 2022 #2022 - 100000 # B.C.E or written as: 2022 - 150 # years
 
 	# Calculate declination values
 	if northOrSouth == "North":
