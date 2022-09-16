b40fd9f3bc9790d18eb13d0b3dc31a7537cd8ee2
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-09-16 16:39:50 -0600

    set up for graphing over longitude and years

--- page_x_astrolabe/calculate_eccentric_calendar_offset.py
+++ page_x_astrolabe/calculate_eccentric_calendar_offset.py
@@ -14,7 +14,7 @@ def determineApside(julianTime):
 
 	return perihelion, aphelion
 
-def determineAngularDistanceEquinox(YYYY, longitude, aphelion):
+def determineAngularDistanceEquinox(julianTime, longitude, aphelion):
 	# Mean Anomaly of January 0
 	mean_anomaly_jan0 = 357.52910 + (35999.0503 * julianTime) - (0.0001559 * (julianTime**2)) - (0.00000048 * (julianTime**3))
 	mean_anomaly_jan0 = mean_anomaly_jan0 % 360 # keep anomaly within 0-360
@@ -24,7 +24,7 @@ def determineAngularDistanceEquinox(YYYY, longitude, aphelion):
 	angular_distance_equinox = aphelion + mean_anomaly_jan0 + (longitude / 365)
 	angular_distance_equinox = angular_distance_equinox % 360
 	angular_distance_equinox -= 360 # reversed from the original position
-	print("\nLine of Apside relative to Vernal Equinox = {0:3f}°".format(angular_distance_equinox))
+	print("\nLine of Apside relative to Vernal Equinox for Longitude {0}° = {1}°".format(longitude, angular_distance_equinox))
 
 	return angular_distance_equinox
 
@@ -39,15 +39,66 @@ def offsetfromCenterOfPlate(radiusOfPlate, perihelion):
 
 	return x_delta, y_delta
 
+def plotYearToOffset():
+	# plot Year vs. Offset for fixed longitude (0°) for radius 1
+	fig = plt.figure(figsize=(10,10), dpi=100)
+	ax = fig.subplots()
+	offset_year = 1000 # (+/-) x years
+	x_year_range = np.arange(2000-offset_year, 2000+offset_year+1, 10)
+
+	y_offset_x = []
+	y_offset_y = []
+	for year in x_year_range:
+		julianTime = (year - 2000) / 100 # Calculate the time in Julian centuries from J2000.0
+		print("Julian Time in Centuries for the Year {0} = {1}".format(yearToCalculate, julianTime))
+		perihelion, aphelion = determineApside(julianTime)
+		vernal_equinox_angle = determineAngularDistanceEquinox(julianTime, longitude, aphelion)
+		x_offset, y_offset = offsetfromCenterOfPlate(radius_of_plate, perihelion)
+		y_offset_x.append(x_offset)
+		y_offset_y.append(y_offset)
+
+	plt.xticks(x_year_range)
+	plt.title("Offset vs. Year")
+	plt.xlabel("Year")
+	plt.ylabel("Offset for fixed longitude (0°) for radius 1")
+	plt.scatter(x_year_range, y_offset_x)
+	plt.scatter(x_year_range, y_offset_y)
+	plt.legend(["X Offset", "Y Offset"])
+	plt.show()
+
+def plotLongitudeToAngularDistance():
+	# plot Year vs. Longitude for fixed year (2022) for radius 1
+	fig = plt.figure(figsize=(10,10), dpi=100)
+	ax = fig.subplots()
+	x_longitude = np.arange(-180, 180+1, 10)
+
+	y_angular_distance_apside = []
+	for longitude in x_longitude:
+		julianTime = (2022 - 2000) / 100 # Calculate the time in Julian centuries from J2000.0
+		print("Julian Time in Centuries for the Year {0} = {1}".format(yearToCalculate, julianTime))
+		perihelion, aphelion = determineApside(julianTime)
+		vernal_equinox_angle = determineAngularDistanceEquinox(julianTime, longitude, aphelion)
+		y_angular_distance_apside.append(vernal_equinox_angle)
+
+	plt.xticks(x_longitude)
+	plt.title("Longitude vs. Angluar Distance to Vernal Equinox")
+	plt.xlabel("Longitude")
+	plt.ylabel("Vernal Equinox Angle (Apside) for fixed year (2022) for radius 1")
+	plt.scatter(x_longitude, y_angular_distance_apside)
+	plt.show()
+
 if __name__ == '__main__':
 	yearToCalculate = 2022
 	longitude = 105.2705 # 105.2705° for Boulder
 	radius_of_plate = 1
+	plotGraphs = True
 
 	# Calculate the time in Julian centuries from J2000.0
 	julianTime = (yearToCalculate - 2000) / 100
-	print("Julian Time in Centuries for the Year {0} = {1}".format(yearToCalculate, julianTime))
-
 	perihelion, aphelion = determineApside(julianTime)
-	vernal_equinox_angle = determineAngularDistanceEquinox(yearToCalculate, longitude, aphelion)
+	vernal_equinox_angle = determineAngularDistanceEquinox(julianTime, longitude, aphelion)
 	x_offset, y_offset = offsetfromCenterOfPlate(radius_of_plate, perihelion)
+
+	if plotGraphs:
+		plotYearToOffset()
+		plotLongitudeToAngularDistance()
